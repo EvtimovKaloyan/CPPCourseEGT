187994dea535426f0a4a3efd77dc0c0f214808b2
EvtimovKaloyan <158089630+EvtimovKaloyan@users.noreply.github.com>
2024-02-01 01:18:05 +0200

    Add files via upload

--- SimpleCarClass/Task.h
+++ SimpleCarClass/Task.h
@@ -1,1 +1,4 @@
-
+* Task 01: CarInfo
+- Create class Car
+- Class object should contain info for make, year, color, fuel, vin, transmission and plate number
+- create get&set methods for all above, as well as display method, showing consolidated info
