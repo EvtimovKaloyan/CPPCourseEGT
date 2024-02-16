91f84d3c998e4bf1f340bb452a4f3e4c163794b3
EvtimovKaloyan <158089630+EvtimovKaloyan@users.noreply.github.com>
2024-02-16 15:21:42 +0200

    Update Circle.cpp

--- CodingTasks_Inheritance_Homework/Circle.cpp
+++ CodingTasks_Inheritance_Homework/Circle.cpp
@@ -1,8 +1,8 @@
 #include "Circle.h"
 
-Circle::Circle(std::string colour) : Shape(colour){}
+Circle::Circle(std::string color) : Shape(color) {}
 
 void Circle::draw() {
-	
-	std::cout << "Drawing a " << this->shapeColour << this->shapeType << std::endl;
+
+	std::cout << "Drawing a " << this->color << " circle" << std::endl;
 }
